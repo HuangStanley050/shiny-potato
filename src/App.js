0ce955a5fcd20ec54e482201010999a701f23da1
Stanley Huang <huang.stanley050@gmail.com>
2020-05-07 12:13:47 +1000

    added check current session and logout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import "./App.css";
 
 import { Auth } from "aws-amplify";
 
+function checkUser() {
+  Auth.currentAuthenticatedUser()
+    .then((user) => console.log({ user }))
+    .catch((err) => console.log(err));
+}
+
+function signOut() {
+  Auth.signOut()
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
+
 function App(props) {
   return (
     <div className="App">
@@ -13,6 +25,8 @@ function App(props) {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+        <button onClick={checkUser}>Current User</button>
+        <button onClikc={signOut}>Logout</button>
       </header>
     </div>
   );
